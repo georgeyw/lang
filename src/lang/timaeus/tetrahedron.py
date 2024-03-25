7ac14a2a4e0d9b96a10b947d6d7233b2159fb22c
George Wang <gwang24@gmail.com>
2024-03-25 17:08:36 +1100

    Fix tetrahedron load

--- src/lang/timaeus/tetrahedron.py
+++ src/lang/timaeus/tetrahedron.py
@@ -1,63 +1,31 @@
-import os
-import json
-
 import torch
-import torch.nn as nn
 from transformer_lens import HookedTransformerConfig, HookedTransformer
 
 from lang.utils import HF_API
 
 
-HF_MODEL_NAME = 'model.pth'
+MODEL_CFG = HookedTransformerConfig(
+    n_layers=2,
+    d_model=256,
+    d_head=32,
+    n_heads=8,
+    n_ctx=1024,
+    d_vocab=5000,
+    tokenizer_name='georgeyw/TinyStories-tokenizer-5k',
+    normalization_type='LN',
+    attn_only=True,
+    seed=1,
+    positional_embedding_type='shortformer',
+)
 
-def load_hf_model(model_name: str, checkpoint_name: str = None) -> nn.Module:
-    repo_id = model_name
-    if not repo_id.startswith(os.environ['HF_AUTHOR']):
-        repo_id = os.environ['HF_AUTHOR'] + '/' + model_name
-    if model_name.startswith(os.environ['HF_AUTHOR']):
-        model_name = model_name.split('/')[1]
-    assert HF_API.token is not None, "Missing HF token"
-    assert _check_model_exists(model_name), f"Model repo at {repo_id} does not exist"
-
-    if checkpoint_name is None:
-        filename = HF_MODEL_NAME
-    else:
-        if not checkpoint_name.endswith('.pth'):
-            checkpoint_name += '.pth'
-        filename = f'checkpoints/{checkpoint_name}'
-    model_path = HF_API.hf_hub_download(repo_id, repo_type='model', filename=filename)
-
-    config = HookedTransformerConfig(**load_hf_config(model_name, 'model'))
-    state_dict = torch.load(model_path)
-
-    model = HookedTransformer(config)
-    model.load_state_dict(state_dict)
-    return model
-
-
-def load_hf_config(model_name: str, config_type: str) -> dict:
-    repo_id = os.environ['HF_AUTHOR'] + '/' + model_name
-    assert HF_API.token is not None, "Missing HF token"
-    assert _check_model_exists(model_name), f"Model repo at {repo_id} does not exist"
-
-    filename = None
-    files = HF_API.list_files_info(repo_id=repo_id, repo_type='model')
-    for file in files:
-        if file.path.startswith(f'configs/{config_type}/'):
-            if filename is not None:
-                raise ValueError(
-                    f"Multiple {config_type} configs found in model repo at {repo_id}.")
-            filename = file.path
-
-    config_path = HF_API.hf_hub_download(repo_id, repo_type='model', filename=filename)
-    with open(config_path, 'r', encoding='utf-8') as f:
-        config = json.load(f)
-        return config
-    
-
-def _check_model_exists(model_name: str) -> bool:
-    models = HF_API.list_models(author=os.environ['HF_AUTHOR'], search=model_name)
-    for model in models:
-        if model.id == os.environ['HF_AUTHOR'] + '/' + model_name:
-            return True
-    return False
+def load_hf_checkpoint(step, n_layers=2):
+  model_cfg = MODEL_CFG
+  model_cfg.n_layers=n_layers
+  model_name = f'L{n_layers}W256-3m-rr'
+  repo_id = f'georgeyw/{model_name}'
+  checkpoint_name = f'checkpoint_{step:0>7d}.pth'
+  model_path = HF_API.hf_hub_download(repo_id, repo_type='model', filename=f'checkpoints/{checkpoint_name}')
+  state_dict = torch.load(model_path)
+  checkpoint = HookedTransformer(model_cfg)
+  checkpoint.load_state_dict(state_dict)
+  return checkpoint
