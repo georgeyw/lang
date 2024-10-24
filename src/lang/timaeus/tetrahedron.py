5f56fb124ac6d36807b96b7a0bfdeea22b1959d6
George Wang <gwang24@gmail.com>
2024-10-24 14:07:27 -0500

    Change HF repo to timaeus

--- src/lang/timaeus/tetrahedron.py
+++ src/lang/timaeus/tetrahedron.py
@@ -22,7 +22,7 @@ def load_hf_checkpoint(step, n_layers=2):
   model_cfg = MODEL_CFG
   model_cfg.n_layers=n_layers
   model_name = f'L{n_layers}W256-3m-rr'
-  repo_id = f'georgeyw/{model_name}'
+  repo_id = f'timaeus/{model_name}'
   checkpoint_name = f'checkpoint_{step:0>7d}.pth'
   model_path = HF_API.hf_hub_download(repo_id, repo_type='model', filename=f'checkpoints/{checkpoint_name}')
   state_dict = torch.load(model_path)
